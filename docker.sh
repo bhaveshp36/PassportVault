aa27f0981c1df8ede7ba5996fb14f759e15418eb
Bhavesh Purohit <bhaveshpurohit994@gmail.com>
2024-05-02 13:48:43 +0530

    update

--- docker.sh
+++ docker.sh
@@ -1,6 +1,5 @@
 docker compose build
 docker compose up
-docker push deadlysin7/passportvault-frontend
-docker push deadlysin7/passportvault-backend
+docker push deadlysin7/passportvault-frontend && docker push deadlysin7/passportvault-backend
 
 
